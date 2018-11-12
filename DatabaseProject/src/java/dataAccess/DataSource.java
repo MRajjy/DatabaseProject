b640641b026f09074db331b765bdf6f73958fa43
kendalhowse <hows0013@algonquincollege.com>
2018-11-12 12:03:07 -0500

    Profile and DataSource files have been completed.

--- DatabaseProject/src/java/dataAccess/DataSource.java
+++ DatabaseProject/src/java/dataAccess/DataSource.java
@@ -5,10 +5,51 @@
  */
 package dataAccess;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Kendal Howse
  */
 public class DataSource {
+
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+    
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+    
+    public void openConnection() {
+        try {
+
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:orcl", "cst8276", "8276");
+            System.out.println("Connection successful!");
+
+        } catch (SQLException e) {
+
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+        }
+    }
+    
+    public void closeConnection() {
+        try {
+            if(connection != null){
+            connection.close();
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        
+    }
     
 }
--- DatabaseProject/src/java/dataTransfer/Profile.java
+++ DatabaseProject/src/java/dataTransfer/Profile.java
@@ -11,4 +11,72 @@ package dataTransfer;
  */
 public class Profile {
     
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String filename;
+    
+    public Profile(){
+        this(0, "", "", "", "", "");
+    }
+    
+    public Profile(int id, String firstName, String lastName, String email, String password, String filename){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.filename = filename;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfilePicture() {
+        return filename;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.filename = profilePicture;
+    }
+    
 }
